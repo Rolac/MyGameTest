bc035724ef139bb1fb1b3d583bd5aac62a327c70
c.giunta <c.giunta@reply.it>
2022-11-21 08:56:57 +0100

    Added Confirm and Reset Button with style - Added Number input field

--- components/PrimaryButton.js
+++ components/PrimaryButton.js
@@ -1,9 +1,49 @@
-import {View, Text} from 'react-native';
+import { View, Text, Pressable, StyleSheet } from 'react-native';
 
-function PrimaryButton({children}) {
-    return <View>
-        <Text>{children}</Text>
-    </View>
+function PrimaryButton({ children }) {
+
+    function pressHandler() {
+        console.log('Pressed!');
+    }
+
+    /** Per mostrare l'effetto ripple dentro il bottone in Android bisogna mettere la View fuorio ed il Pressable dentro */
+    return (
+        <View style={styles.buttonOuterContainer}>
+            {/** L'evento pressed è un booleano generato dall'oggetto Pressable*/}
+            <Pressable
+                onPress={pressHandler}
+                android_ripple={{ color: '#640233' }}
+                style={({pressed}) => pressed 
+                ? [styles.buttonInnerContainer, styles.pressed] 
+                : styles.buttonInnerContainer }>
+                <Text style={styles.buttonText}>{children}</Text>
+            </Pressable>
+        </View>
+    );
+
 }
+
+const styles = StyleSheet.create({
+    buttonOuterContainer: {
+        borderRadius: 28,
+        margin: 4,
+        overflow: 'hidden'
+    },
+    buttonInnerContainer: {
+        backgroundColor: '#72063c',
+        paddingVertical: 8,
+        paddingHorizontal: 16,
+        elevation: 2
+    },
+    buttonText: {
+        color: 'white',
+        textAlign: 'center'
+    },
+    /** Per l'effetto su iOS */
+    pressed: {
+        opacity: 0.75,
+    }
+
+});
 
 export default PrimaryButton;
