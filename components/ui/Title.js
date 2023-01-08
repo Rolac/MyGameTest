409f277f1ca221c73e6ee4aa8eb4b53f9c469f76
carlo giunta <giunta.carlo92@gmail.com>
2023-01-08 11:42:33 +0100

    Added different platform code

--- components/ui/Title.js
+++ components/ui/Title.js
@@ -1,6 +1,13 @@
-import { Text, StyleSheet } from 'react-native';
+import { Text, StyleSheet, PLatform } from 'react-native';
 import Colors from '../../constants/colors';
 
+/*
+è possibile creare componenti specifici per piattaforma semplicemente aggiungedo .ios o .android al nome del file
+Per esempio per questo file si può aggiungere:
+Title.ios.js
+Title.android.js
+per gli import però bisogna mantenere soltanto Title.js
+*/
 function Title({children}) {
     return   (
         <Text style={styles.title}>{children}</Text>
@@ -13,7 +20,8 @@ const styles = StyleSheet.create({
         fontFamily: 'open-sans-bold', 
         color: Colors.title,
         textAlign: 'center',
-        borderWidth: 2,
+        // borderWidth: Platform.OS === 'android' ? 2 : 0,
+        borderWidth: Platform.select({ios: 0, android: 2}),
         borderColor: Colors.title,
         padding: 12,
         maxWidth: '80%',
